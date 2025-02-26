9e776052689178eccdc183cc7287b6b20a46d811
i10416 <ito.yo16uh90616@gmail.com>
2025-02-26 14:48:22 +0900

    tidy: use Visitor Translator for simplicity

---
+++ Sources/QueryTranslator/QueryVisitorTranslator.swift
@@ -0,0 +1,45 @@
+import Antlr4
+import Core
+import QueryParser
+
+public final class QueryVisitorTranslator: QueryBaseVisitor<Expr> {
+    public override func visitExpr(_ ctx: QueryParser.ExprContext) -> Expr? {
+        if ctx.OR_OP() != nil {
+            let expr = visitExpr(ctx.expr()!)!
+            let term = visitTerm(ctx.term()!)!
+            return Expr.orExpr(expr, term)
+        } else {
+            return visitTerm(ctx.term()!)
+        }
+    }
+    public override func visitTerm(_ ctx: QueryParser.TermContext) -> Expr? {
+        if ctx.AND_OP() != nil {
+            let term = visitTerm(ctx.term()!)!
+            let factor = visitFactor(ctx.factor()!)!
+            return Expr.andExpr(term, factor)
+        } else {
+            return visitFactor(ctx.factor()!)
+        }
+    }
+    public override func visitFactor(_ ctx: QueryParser.FactorContext) -> Expr? {
+        if ctx.NOT_OP() != nil {
+            let keywords = visitKeywords(ctx.keywords()!)!
+            return Expr.notExpr(keywords)
+        } else {
+            return visitKeywords(ctx.keywords()!)
+        }
+    }
+
+    public override func visitKeywords(_ ctx: QueryParser.KeywordsContext) -> Expr? {
+        switch ctx.getChildCount() {
+        case 3:
+            return visitExpr(ctx.expr()!)
+        default:
+            return visitKeyword(ctx.keyword()!)
+        }
+    }
+    public override func visitKeyword(_ ctx: QueryParser.KeywordContext) -> Expr? {
+        return Expr.keyword(ctx.getText())
+    }
+
+}
